8c55792edf07b8bbbcdaedd561b71ba9bbe2290f
ViterbiDevelopment <wangchaonihao@163.com>
2017-12-12 22:49:11 +0800

    scrapy babyname in webName

--- tutorial/tutorial/spiders/QuotesSpider.py
+++ tutorial/tutorial/spiders/QuotesSpider.py
@@ -1,15 +1,30 @@
 import scrapy
+import os
 
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
-    start_urls = [
-        'http://wang.resgain.net/name/boys.html',
-    ]
+    start_urls = []
+    #http://wang.resgain.net/name/boys_2.html
+    for x in range(1,11):
+    	url = 'http://wang.resgain.net/name/boys_%d.html' % x
+    	start_urls.append(url)
+
+    for x in range(1,11):
+    	url = 'http://wang.resgain.net/name/girls_%d.html' % x
+    	start_urls.append(url)
 
     def parse(self, response):
         #col-xs-12. //div[@class="col-xs-12"]. extract_first()
+        file = response.url.split('/')[4]
+        filename = file.split('_')[0]
+        print filename
+        f = open(filename+'.text','a')
         for sel in response.xpath('//div[@class="main_"]'):
         	for name in sel.xpath('.//div[@class="col-xs-12"]'):
-        		print name.xpath('./a[contains(@href,"html")]/@href').extract()
+        		sel = name.xpath('./a[contains(@href,"html")]/@href').extract()
+        		for detail in sel:
+        			babyname = detail.encode('utf-8').split('/')[2].split('.')[0]
+        			f.write(babyname + '\n')
+        f.close()
